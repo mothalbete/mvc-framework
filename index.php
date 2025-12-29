6c0ef1dcedf6eaa76c45ac701071de5e6449a98b
tfermoso <tomas.fermoso@gmail.com>
2025-12-29 16:22:16 +0100

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to the MVC Framework</h1>
+    <?php
+    echo $_GET["url"];
+    ?>
+
+</body>
+</html>
